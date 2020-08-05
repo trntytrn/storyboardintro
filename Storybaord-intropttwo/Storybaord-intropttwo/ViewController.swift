663b3ad18b5fc11e608f0dcfff6128940fc6a13f
Trinity Tran <41582245+trntytrn@users.noreply.github.com>
2020-08-05 11:35:38 -0400

    first push

---
+++ Storybaord-intropttwo/Storybaord-intropttwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storybaord-intropttwo
+//
+//  Created by trinity on 8/5/20.
+//  Copyright © 2020 trinity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
