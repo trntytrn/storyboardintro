c5198985c9421d82977c6dd6a57b8d54f2b3ffad
Trinity Tran <41582245+trntytrn@users.noreply.github.com>
2020-08-05 15:04:10 -0400

    outlets

--- Storybaord-intropttwo/Storybaord-intropttwo/ViewController.swift
+++ Storybaord-intropttwo/Storybaord-intropttwo/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var textField: UITextField!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("this is a print statement")
     }
-
-
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if let newTitle = textField.text {
+            appTitle.text = newTitle
+        }
+    }
+    
 }
+
 
